8a88e37c9955c77e89399dd1f36d9a1850eaeea4
shaun <shauna.pai@gmail.com>
2025-03-01 17:16:21 +0530

    Fix: NVIM LSP Config

--- home/modules/neovim/nvim/lua/plugins/rust_lsp.lua
+++ home/modules/neovim/nvim/lua/plugins/rust_lsp.lua
@@ -1,24 +1,26 @@
-return {
-    setup = function()
-        local lspconfig = require("lspconfig")
+local M = {}
 
-        lspconfig.rust_analyzer.setup({
-            cmd = { "rust-analyzer" }, -- Uses the system-installed rust-analyzer
-            filetypes = { "rust" },
-            root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
-            settings = {
-                ["rust-analyzer"] = {
-                    checkOnSave = {
-                        command = "clippy",
-                    },
-                    cargo = {
-                        allFeatures = true,
-                    },
-                    procMacro = {
-                        enable = true,
-                    },
+M.setup = function()
+    local lspconfig = require("lspconfig")
+
+    lspconfig.rust_analyzer.setup({
+        cmd = { "rust-analyzer" }, -- Uses system-installed rust-analyzer
+        filetypes = { "rust" },
+        root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
+        settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy",
+                },
+                cargo = {
+                    allFeatures = true,
+                },
+                procMacro = {
+                    enable = true,
                 },
             },
-        })
-    end
-}
+        },
+    })
+end
+
+return M
