10df0d8f13aefaf924534d5119e5b3a0a50a1b27
shaun <shauna.pai@gmail.com>
2025-03-02 18:15:33 +0530

    Fix: Nvim lsp config changed

--- home/modules/neovim/nvim/lua/plugins/copilot.lua
+++ home/modules/neovim/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,53 @@
 return {
     {
         "zbirenbaum/copilot.lua",
-        enabled = false,
-        event = "BufEnter",
+        enabled = true,
+        event = "InsertEnter", -- Load Copilot only when needed
         opts = {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
+            panel = {
+                enabled = true,
+                auto_refresh = false, -- Prevents unnecessary updates
+                keymap = {
+                    open = "<M-CR>", -- Meta+Enter opens Copilot panel
+                    refresh = "gr", -- Refresh Copilot suggestions
+                    accept = "<CR>", -- Accept suggestion
+                },
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true, -- Enables inline suggestions automatically
+                debounce = 75, -- Slight delay for better performance
+                keymap = {
+                    accept = "<Tab>", -- Accept current suggestion
+                    next = "<M-]>", -- Next suggestion
+                    prev = "<M-[>", -- Previous suggestion
+                    dismiss = "<C-]>",
+                },
+            },
+            filetypes = {
+                markdown = true, -- Enable Copilot for Markdown
+                yaml = true, -- Enable Copilot for YAML
+                gitcommit = true, -- Enable for Git commits
+                lua = true,
+                rust = true,
+                go = true,
+                python = true,
+                ["*"] = false, -- Disable by default for all other filetypes
+            },
         },
     },
     {
         "zbirenbaum/copilot-cmp",
-        enabled = false,
-        opts = {}
-    }
+        enabled = true,
+        dependencies = { "hrsh7th/nvim-cmp" }, -- Ensure nvim-cmp is loaded
+        config = function()
+            require("copilot_cmp").setup({
+                method = "getCompletionsCycling", -- Cycles through Copilot suggestions
+                formatters = {
+                    insert_text = require("copilot_cmp.format").remove_existing,
+                },
+            })
+        end,
+    },
 }
+
