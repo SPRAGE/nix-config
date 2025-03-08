f88e090a3c9c8bb4eb5396aebdc75fbe32548caf
shaun <shauna.pai@gmail.com>
2025-03-08 08:11:57 +0530

    Feat: Added Greetd

--- home/shaun/modules/default.nix
+++ home/shaun/modules/default.nix
@@ -5,7 +5,6 @@
     ../../modules/fonts.nix
     ../../modules/kitty.nix
     # ../../modules/hyprland.nix
-    ../../modules/greetd.nix
     ../../modules/sway.nix
     ../../modules/nix-flake-templates
 
