d8bbf85a7f85c8eeb1d8116a061c2e3489aca89b
shaun <shauna.pai@gmail.com>
2025-03-12 08:22:40 +0530

    Feat: NIXVIM switch

--- flake.nix
+++ flake.nix
@@ -17,6 +17,15 @@
       # on a binary cache.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+    # url = "github:nix-community/nixvim";
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    url = "github:nix-community/nixvim/nixos-24.11";
+
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+
   };
 
   outputs = { self, home-manager, nixpkgs,nvf, ... }@inputs:
