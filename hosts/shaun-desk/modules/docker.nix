e2ab88a5a43b48e61237d06ee7972a5d90737487
shaun <shauna.pai@gmail.com>
2025-03-16 16:04:24 +0530

    Feat: Docker

--- hosts/shaun-desk/modules/docker.nix
+++ hosts/shaun-desk/modules/docker.nix
@@ -3,7 +3,9 @@
 
 {
   # Import Docker’s NixOS module to get default Docker configuration options
-  imports = [ <nixpkgs/nixos/modules/virtualisation/docker.nix> ];
+  imports = [
+    (import "${pkgs.path}/nixos/modules/virtualisation/docker.nix")
+  ];
 
   # Enable the Docker service
   services.docker.enable = true;
@@ -11,7 +13,5 @@
   # Install Docker client and additional tools as part of the system packages
   environment.systemPackages = with pkgs; [ docker ];
 
-  # (Optional) Allow your user to run Docker commands without sudo.
-  # Replace "yourusername" with your actual username.
-  users.users.shaun.extraGroups = [ "docker" ];
+ 
 }
