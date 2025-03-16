b043318f621c061796cfe2371f94afdc1aac2334
shaun <shauna.pai@gmail.com>
2025-03-16 16:10:47 +0530

    Feat: Docker

--- hosts/shaun-desk/modules/docker.nix
+++ hosts/shaun-desk/modules/docker.nix
@@ -3,12 +3,8 @@
 
 {
   # Import Docker’s NixOS module to get default Docker configuration options
-  imports = [
-    (import "${pkgs.path}/nixos/modules/virtualisation/docker.nix")
-  ];
-
-  # Enable the Docker service
-  services.docker.enable = true;
+  virtualisation.docker.enable = true;
+  
 
   # Install Docker client and additional tools as part of the system packages
   environment.systemPackages = with pkgs; [ docker ];
