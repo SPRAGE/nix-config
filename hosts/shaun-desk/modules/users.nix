6cb158af0ca6ebaf80d0ab0e91a1612f9e46a78c
shaun <shauna.pai@gmail.com>
2025-03-16 16:03:28 +0530

    Feat: Docker

--- hosts/shaun-desk/modules/users.nix
+++ hosts/shaun-desk/modules/users.nix
@@ -3,7 +3,7 @@
 {
   users.users.shaun = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "audio" ];
+    extraGroups = [ "wheel" "networkmanager" "audio" "docker" ];
     # shell = pkgs.zsh;
   };
   nix.settings.trusted-users = [ "shaun" ]; 
